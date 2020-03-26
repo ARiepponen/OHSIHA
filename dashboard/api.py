738ee834c4f67c69f705e6701c4804fee01cad57
ARiepponen <antti.riepponen@hotmail.fi>
2020-03-26 13:11:46 +0200

    Tiedon lukeminen netistä ja tallentaminen listoihin

--- dashboard/api.py
+++ dashboard/api.py
@@ -3,6 +3,35 @@ from alpha_vantage.timeseries import TimeSeries
 import time
 api_key = 'G52RTNSQBOQ4FZZY'
 
-ts = TimeSeries(key = api_key, output_format='pandas')
-data = ts.get_daily(symbol='MSFT', outputsize = 'full')
-print(data)
+ts = TimeSeries(key = api_key, output_format='json')
+data = ts.get_daily(symbol='MSFT', outputsize = 'compact')
+
+paivat = []
+kurssit = []
+
+for key in data:
+    counter = 0
+    for paiva in key.keys():
+
+        if(counter <=85):  
+            paivat.append(paiva)
+
+            arvo = key[paiva]["1. open"]
+            print(arvo)
+            kurssit.append(arvo)
+            counter += 1
+
+
+
+
+
+
+
+   # for arvo in key.values():
+        #kurssi.append(arvo)
+
+#for arvo in paivat:
+   # print(arvo)
+
+for kurssi in kurssit:
+    print(kurssi)
