0a3611552fc6557d9f470fc70a94b69225f6dde6
ARiepponen <antti.riepponen@hotmail.fi>
2020-03-28 11:46:08 +0200

    Backup api kokeilun vuoksi

--- dashboard/api.py
+++ dashboard/api.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 import time
+
+
 api_key = 'G52RTNSQBOQ4FZZY'
 
 ts = TimeSeries(key = api_key, output_format='json')
@@ -8,30 +10,32 @@ data = ts.get_daily(symbol='MSFT', outputsize = 'compact')
 
 paivat = []
 kurssit = []
+counter = 0
 
 for key in data:
-    counter = 0
-    for paiva in key.keys():
+    counter = counter + 1
+    if(counter <= 5):
+        for paiva in key.keys():
 
-        if(counter <=85):  
             paivat.append(paiva)
+            try:       
+                arvo = key[paiva]["1. open"]
+                
+                kurssit.append(arvo)
+            except:
+                pass
 
-            arvo = key[paiva]["1. open"]
-            print(arvo)
-            kurssit.append(arvo)
-            counter += 1
+pituus = len(paivat)
+uusi_pituus = pituus - 5
+del paivat[uusi_pituus:]
 
-
-
-
-
-
-
-   # for arvo in key.values():
-        #kurssi.append(arvo)
-
-#for arvo in paivat:
-   # print(arvo)
 
 for kurssi in kurssit:
     print(kurssi)
+
+for dagen in paivat:
+    print(dagen)
+
+
+print(len(paivat))
+print(len(kurssit))
--- dashboard/models.py
+++ dashboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Bond(models.Model):
     description = models.CharField(max_length=200)
-    buying_price = models.DecimalField(max_digits=8,decimal_places=2)
+    buying_price = models.IntegerField()
     total_price = models.IntegerField()
    
 
