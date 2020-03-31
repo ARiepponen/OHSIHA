700d0b8b49c3b6cd819cc940b5e86028b318df45
ARiepponen <antti.riepponen@hotmail.fi>
2020-03-31 22:48:55 +0300

    Tyylit laitettu aluille & Oma tiedosto toimimattomille osakkeille

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_bonds, create_bond, update_bond, delete_bond
+from .views import list_bonds, create_bond, update_bond, delete_bond, not_in_database
 from .data import get_data
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('new', create_bond,name='create_bond'),
     path('update/<int:id>', update_bond, name='update_bond'),
     path('delete/<int:id>', delete_bond, name='delete_bond'),
+    path('not_found/<int:id>', not_in_database, name='not_found')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,6 +5,7 @@ from .forms import BondForm
 from alpha_vantage.timeseries import TimeSeries
 import time
 
+#bond_list = []
 
 
 def list_bonds(request):
@@ -23,38 +24,57 @@ def create_bond(request):
 def update_bond(request, id):
     bond = Bond.objects.get(id=id)
     form = BondForm(request.POST or None, instance = bond)
-
-
     api_key = 'G52RTNSQBOQ4FZZY'
 
-    ts = TimeSeries(key = api_key, output_format='json')
-    data = ts.get_daily(symbol=bond, outputsize = 'compact')
+    try:
+        ts = TimeSeries(key = api_key, output_format='json')
+        data = ts.get_daily(symbol=bond, outputsize = 'compact')
 
-    paivat = []
-    avaimet = []
+        paivat = []
+        avaimet = []
+        #if bond not in bond_list:
+            #bond_list.append(bond)
+            
+        for rivi in data:
+            for paiva in rivi.keys():
+                print(paiva)
+                avain = rivi[paiva]
+                avaimet.append(avain)
 
-    
-    
-    for rivi in data:
-        for paiva in rivi.keys():
-            print(paiva)
-            avain = rivi[paiva]
-            avaimet.append(avain)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('list_bonds')
+        return render(request, 'bonds-form.html', {'form':form, 'bond':bond, 'paivat':paivat, 'avaimet':avaimet})
 
-    
-    if form.is_valid():
-        form.save()
-        return redirect('list_bonds')
-    return render(request, 'bonds-form.html', {'form':form, 'bond':bond, 'paivat':paivat, 'avaimet':avaimet})
-
-
+    except: 
+        print("papukaija")
+        return render(request, 'bond_not_found.html', {'bond':bond})
+     
 
 def delete_bond(request, id):
     bond = Bond.objects.get(id=id)
-
+    
     if request.method == 'POST':
+        print(bond)
         bond.delete()
+        
         return redirect('list_bonds')
-
+    
     return render(request, 'bond-delete.html', {'bond':bond})
 
+
+
+def not_in_database(request, id):
+    bond = Bond.objects.get(id=id)
+
+    if request == 'GET':
+        bond.delete()
+        print("kissa")
+        return redirect('list_bonds')
+    
+    print("koira")
+
+    return redirect(request, 'bond_not_found.html', {'bond':bond})
+    
+    
