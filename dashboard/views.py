f5ef941f663ce33dd3bcad7b177495578ddecd29
ARiepponen <antti.riepponen@hotmail.fi>
2020-04-07 16:36:40 +0300

    Kuvaajat valmiina

--- dashboard/views.py
+++ dashboard/views.py
@@ -31,24 +31,55 @@ def update_bond(request, id):
         data = ts.get_daily(symbol=bond, outputsize = 'compact')
 
         paivat = []
+        paivatrvrs = []
         avaimet = []
-        #if bond not in bond_list:
-            #bond_list.append(bond)
-            
+        open_values = []
+        volume_values = []
+
+        paivastr = "["
+        k = 0 
         for rivi in data:
             for paiva in rivi.keys():
-                print(paiva)
+                if(k<=99):
+                    paivat.append(paiva)
+                    paivatrvrs.append(paiva)
+                    if(k>0):
+                        paivastr = paivastr + ", " + paiva
+                    if(k<1):
+                        paivastr = paivastr + paiva
+
                 avain = rivi[paiva]
                 avaimet.append(avain)
+                k += 1
+        paivastr = paivastr + "]"
+        i = 0
+        volumestr = "["
 
+        for arvo in avaimet:
+            if(i<=99):
+                open_values.append(arvo['1. open'])
+                volume_values.append(int(arvo['5. volume']))
+                if(i>0):
+                    volumestr = volumestr + ", " + arvo['5. volume']
+                elif(i<1):
+                    volumestr = volumestr + arvo['5. volume']
         
+                
+            i += 1
+        volumestr = volumestr + "]"
+        paivatrvrs.reverse()
+        volume_values.reverse()
+        open_values.reverse()
+        
+        testi = str('kissa')
         if form.is_valid():
             form.save()
             return redirect('list_bonds')
-        return render(request, 'bonds-form.html', {'form':form, 'bond':bond, 'paivat':paivat, 'avaimet':avaimet})
+        return render(request, 'bonds-form.html', {'form':form, 'bond':bond, 'paivat':paivat, 
+        'avaimet':avaimet, 'open_values':open_values,
+        'volume_values':volume_values, 'paivastr':paivastr, 'volumestr':volumestr,'testi':testi, 'paivatrvrs':paivatrvrs})
 
-    except: 
-        print("papukaija")
+    except:     
         return render(request, 'bond_not_found.html', {'bond':bond})
      
 
